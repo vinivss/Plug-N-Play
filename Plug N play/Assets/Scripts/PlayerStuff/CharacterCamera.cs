bc5a2166361d2c673823a84bd309390d940dd3b3
thebeardlessdwarf <vinivss@hotmail.com>
2021-03-01 18:44:42 -0800

    new stuff

---
+++ Plug N play/Assets/Scripts/PlayerStuff/CharacterCamera.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterCamera : MonoBehaviour
+{
+    float yawCamera;
+    //public CharacterController controller;
+
+    public float turnspeed = 15;
+        Camera mainCamera;
+    // Start is called before the first frame update
+    void Start()
+    {
+        mainCamera = Camera.main;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+         yawCamera = mainCamera.transform.rotation.eulerAngles.y;
+        //Vector3 Velo = controller.velocity;
+
+        //Velo = new Vector3(controller.velocity.x, controller.velocity.y, controller.velocity.z);
+
+        //float speed = Velo.magnitude;
+
+        //if(speed >= 0f)
+        //{
+
+        //    Debug.Log("isread");
+
+        //transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.Euler(0, yawCamera, 0), turnspeed * Time.fixedDeltaTime);
+
+        //}
+
+    }
+
+     void Awake()
+    {
+
+        
+    }
+}
---
+++ Plug N play/Assets/Scripts/PlayerStuff/TotemState.cs
@@ -0,0 +1,34 @@
+﻿ using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+namespace Jupiter
+{
+    public class TotemState : MonoBehaviour
+    {
+        GameManager Manager;
+       public GameObject Totem0;
+       public GameObject Totem1;
+       public GameObject Cam0;
+       public GameObject Cam1;
+
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            Manager = GameObject.Find("GameManager").GetComponent<GameManager>();
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            if (Manager.Climber)
+            {
+                Cam0.SetActive(false);
+                Cam1.SetActive(true);
+                Totem0.SetActive(false);
+                Totem1.SetActive(true);
+                Totem1.transform.position = Totem0.transform.position;
+            }
+        }
+    }
+}
