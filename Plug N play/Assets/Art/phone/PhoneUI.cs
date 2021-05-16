efe4c4bee19c914fe71637962613830da328f42f
thebeardlessdwarf <vinivss@hotmail.com>
2021-05-15 17:22:39 -0700

    WE

---
+++ Plug N play/Assets/Art/phone/PhoneUI.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+namespace Squid
+{
+    public class PhoneUI : MonoBehaviour
+    {
+        public GameObject lockscreen;
+        public GameObject sslider;
+        public Slider slider;
+        public AsyncScene cont;
+        public void Awake()
+        {
+            cont = GameObject.FindObjectOfType<AsyncScene>();
+        }
+        public void Unlock()
+        {
+            if (slider.value >= 1)
+            {
+
+                sslider.SetActive(false);
+                lockscreen.SetActive(false);
+            }
+
+        }
+
+        public void ExitOut()
+        {
+            sslider.SetActive(true);
+            lockscreen.SetActive(true);
+
+            SceneManager.UnloadSceneAsync(SceneManager.GetSceneByName("Phone"));
+            cont.becomeActive();
+        }
+    }
+}
