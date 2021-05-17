b80ddf4f9663763480b2263784b28bb7cfafd654
thebeardlessdwarf <vinivss@hotmail.com>
2021-05-16 21:42:18 -0700

    ui in

---
+++ Plug N play/Assets/Art/UI icons/USBANIm.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class USBANIm : MonoBehaviour
+{
+    public Animator anim;
+    public GameManager manager;
+
+    private void Awake()
+    {
+        manager = GameObject.Find("GameManager").GetComponent<GameManager>();
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if(manager.Climber)
+        {
+            anim.SetBool("Climber-kun", true);
+        }
+
+        if(manager.USBmini)
+        {
+            anim.SetBool("Type C san", true);
+        }
+    }
+}
